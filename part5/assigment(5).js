eb04c24487ea812d4efc010f8b06b4f703e2b900
NikitaDafda <dafdanikita0@gmail.com>
2026-02-19 19:36:44 +0530

    object literals

---
+++ part5/assigment(5).js
@@ -0,0 +1,26 @@
+//Q1    generate random num btw 1 to 6
+
+let random=Math.floor(Math.random()*6)+1;
+console.log(random);
+
+
+//Q2
+
+const car={
+    carName:"farari",
+    model:2025,
+    color:"black"
+};
+
+car.carName;
+
+//Q3
+
+const person={
+    name:"xyz",
+    age:105,
+    city:"hachuba"
+};
+
+person.city="new york";
+person.country="USA";
---
+++ part5/objectliterals.js
@@ -0,0 +1,22 @@
+const post={
+            username:"@nikitaDAfda",
+            likes:200,
+            repost:3,
+            view:"1M"
+            
+};
+
+//Array of obj
+
+const student=[
+               { name:"nikita",
+                roll:1,
+                marks:50
+               },
+               {
+                name:"tushar",
+                roll:2,
+                marks:50
+               }
+        
+]
