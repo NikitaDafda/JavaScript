4d18dce2f3cc1ecd546214cda5eb626b50a372d3
NikitaDafda <dafdanikita0@gmail.com>
2026-03-04 21:34:35 +0530

    use spread

---
+++ spread.js
@@ -0,0 +1,21 @@
+
+let arr=[1,5,3,6,8,9,2,0];
+
+Math.min(...arr);
+
+//merge 2 array
+
+let odd=[1,3,5,7,9];
+let even=[2,4,6,8,10];
+let newArr=[...odd,...even];
+
+let str=[..."hello"];
+
+//object literls
+
+let data={
+    gmail:"nikita@gmail.com",
+    password:"abcd"
+};
+
+let copydata={...data,id:123};
